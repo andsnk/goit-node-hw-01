fce4469002459bf97304dd4fb3cafde675e70896
andsnk <127045868+andsnk@users.noreply.github.com>
2023-11-22 21:30:58 +0200

    err fix

--- index.js
+++ index.js
@@ -13,31 +13,34 @@ program.parse(process.argv);
 const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      const allContacts = await contacts.listContacts();
-      console.log(allContacts);
-      break;
+  try {
+    switch (action) {
+      case "list":
+        const allContacts = await contacts.listContacts();
+        console.log(allContacts);
+        break;
 
-    case "get":
-      const contact = await contacts.getContactById(id);
-      console.log(contact);
-      break;
+      case "get":
+        const contact = await contacts.getContactById(id);
+        console.log(contact);
+        break;
 
-    case "add":
-      const newContact = await contacts.addContact(name, email, phone);
-      console.log(newContact);
-      break;
+      case "add":
+        const newContact = await contacts.addContact(name, email, phone);
+        console.log(newContact);
+        break;
 
-    case "remove":
-      const removedContact = await contacts.removeContact(id);
-      console.log(removedContact);
-      break;
+      case "remove":
+        const removedContact = await contacts.removeContact(id);
+        console.log(removedContact);
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
   }
 }
 
 invokeAction(argv);
-
